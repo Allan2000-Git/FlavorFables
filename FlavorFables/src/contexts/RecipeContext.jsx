435b533336571fc0b3bd2a6b9e79995226c1d95d
Allan Joston Fernandes <54631653+Allan2000-Git@users.noreply.github.com>
2023-10-19 21:40:05 +0530

    Update RecipeContext.jsx

--- FlavorFables/src/contexts/RecipeContext.jsx
+++ FlavorFables/src/contexts/RecipeContext.jsx
@@ -10,8 +10,8 @@ export const RecipeContextProvider = ({children}) => {
     const [searchInput, setSearchInput] = useState("Chicken");
 
     const fetchData = async (query) => {
-        const REACT_APP_EDAMAM_APP_ID = "75ee2d72"
-        const REACT_APP_EDAMAM_API_KEY = "f779d4a7076748c9827cbf91bba7228c"
+        const REACT_APP_EDAMAM_APP_ID = "YOU_API_ID"
+        const REACT_APP_EDAMAM_API_KEY = "YOU_API_KEY"
 
         const url = `https://api.edamam.com/search?q=${query}&app_id=${REACT_APP_EDAMAM_APP_ID}&app_key=${REACT_APP_EDAMAM_API_KEY}&from=0&to=${limit}`;
         
